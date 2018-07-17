837453e8c144e4713d908ca24d01b53d38a03f64
Erik Mikas Subatis <erik.subatis@gmail.com>
2018-07-17 23:38:19 +0000

    updated channel list to use socket IO instead of AJAX directly

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import Flask, render_template, request, session, jsonify
 from flask_session import Session
@@ -13,25 +14,33 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# list of all channels
+#### TODO: store messages
+
+# list of channels
 channel_list = ['general']
+
+# Message class
+class Message:
+    def __init__(self, msg, timestamp):
+        self.msg = msg;
+        self.timestamp = datetime.datetime.now()
 
 # Main route
 @app.route("/")
 def index():
     return render_template("index.html")
 
-# Adding a new channel; returns the channel list for rendering
-@app.route("/new_channel", methods=["POST"])
-def new_channel():
-    print ("new channel route called")
+# Adding a new channel; receive event and emit channel list for rendering
+@socketio.on("create_channel")
+def create_channel(data):
+    print("Socket IO: create channel called")
 
-    new_channel = request.form.get("channel_name")
-    channel_list.append(new_channel)
+    new_channel = data["channel_name"]
+    channel_list.append(data["channel_name"])
 
-    print(f"/new_channel added {new_channel}")
+    print(f"Appended channel {new_channel}")
 
-    return jsonify(channel_list)
+    emit("channel_created", channel_list, broadcast=True)
 
 # Set username; sets session name
 @app.route("/set_username", methods=["POST"])
@@ -43,3 +52,8 @@ def set_username():
     print(f"/set_username set username {session['username']}")
 
     return "Success"
+
+# Get channel list
+@app.route("/get_channels")
+def get_channels():
+    return jsonify(channel_list);
--- static/chat.js
+++ static/chat.js
@@ -1,5 +1,31 @@
 // Function definitions
 
+// Display channels
+function showChannels(channels) {
+    let channelList = '';
+
+    for (var i = 0; i < channels.length; ++i) {
+        channelList += '<a href="#" class="list-group-item list-group-item-action">';
+        channelList += channels[i];
+        channelList += '</a>';
+    }
+
+    document.querySelector('#channel-list').innerHTML = channelList;
+
+    // Set change channel functionality to each list item
+    document.querySelectorAll('.list-group-item-action').forEach(function(channel) {
+        channel.onclick = function() {
+            localStorage.setItem('current_channel', this.innerHTML);
+            showChat();
+        };
+    });
+}
+
+// Show channel chat
+function showChat() {
+    document.querySelector('#channel-display').innerHTML = localStorage.getItem('current_channel');
+}
+
 // Load main form/page - must get channel data from application.py
 
 // Change channel - must get channel data from application.py (socket/AJAX)
@@ -8,15 +34,61 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Connect to socket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // Socket connect - configure applicable buttons & forms
+    socket.on('connect', () => {
+
+        // Create channel
+        document.querySelector('#new-channel-form').onsubmit = () => {
+            // Get new channel name
+            var newChannel = document.querySelector('#txt-new-channel').value;
+
+            // Emit event to Flask with channel name
+            socket.emit('create_channel', {'channel_name': newChannel});
+
+            return false;
+        };
+    });
+
+    // Socket - listen for changes to channel list
+    socket.on('channel_created', data => {
+        showChannels(data)
+    });
+
+    // Show username if it exists
+    if (localStorage.getItem('username')) {
+        document.querySelector('#username-display').innerHTML = `, ${localStorage.getItem('username')}`;
+    }
+
+    // Set channel to general if no current channel
+    if (!localStorage.getItem('current_channel')) {
+        localStorage.setItem('current_channel', 'general');
+    }
+
+    // Show current chat
+    showChat();
+
+    // Display current channels
+    const request = new XMLHttpRequest();
+    request.open('GET', '/get_channels');
+    request.onload = () => {
+        const data = JSON.parse(request.responseText);
+        showChannels(data);
+    };
+    request.send();
+
+
     // Set page events
 
     // Set username
     document.querySelector('#username-form').onsubmit = () => {
         // Get username and set local storage
-        var username = document.querySelector('#txt-username').value;
+        username = document.querySelector('#txt-username').value;
         localStorage.setItem('username', username);
 
-        // Start and send new AJAX request with username data
+        // Start and send new AJAX request with username data TODO MAY NOT NEED
         const request = new XMLHttpRequest();
         request.open('POST', '/set_username');
         request.onload = () => {
@@ -24,35 +96,6 @@ document.addEventListener('DOMContentLoaded', () => {
         };
         const data = new FormData();
         data.append('username', username);
-        request.send(data);
-
-        return false;
-    };
-
-    // Create channel
-    document.querySelector('#new-channel-form').onsubmit = () => {
-        // Get new channel name and send to Flask via AJAX
-        var newChannel = document.querySelector('#txt-new-channel').value;
-
-        const request = new XMLHttpRequest();
-        request.open('POST', '/new_channel');
-
-        // Refresh channel list
-        request.onload = () => {
-            const data = JSON.parse(request.responseText);
-            let channelList = '';
-
-            for (var i = 0; i < data.length; ++i) {
-                channelList += '<a href="#" class="list-group-item list-group-item-action">';
-                channelList += data[i];
-                channelList += '</a>';
-            }
-
-            document.querySelector('#channel-list').innerHTML = channelList;
-        };
-
-        const data = new FormData();
-        data.append('channel_name', newChannel);
         request.send(data);
 
         return false;
